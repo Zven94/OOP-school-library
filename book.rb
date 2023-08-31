41dcbe4fdedbb57d15356755317aac50bcf8a4bf
Nicolas Bolañez <Zven94>
2023-08-31 19:22:14 -0300

    fix linters errors

--- book.rb
+++ book.rb
@@ -1,10 +1,10 @@
 # define Book class
 class Book
-  attr_accessor :title,:author
+  attr_accessor :title, :author
   attr_reader :rental
-  def initialize(title,author)
+
+  def initialize(title, author)
     @title = title
     @author = author
-    @rental
   end
 end
